873e3f7892adf289b33a87403b52d2730298ca09
Vicolas <38006170+Vicolas@users.noreply.github.com>
2022-09-26 00:09:43 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 export const CONFIG = {
   TEMPLATE: {
     // 1. Set your NFT collection contract address
-    "collection_address": "0x9498274b8c82b4a3127d67839f2127f2ae9753f4",
+    "collection_address": "0xcB8d1260F9c92A3A545d409466280fFdD7AF7042",
 
     // 2. Set your blockchain chain ID where your NFT collection contract address is deployed (see below for value options)
-    "block_chain_id": "137",
+    "block_chain_id": "1",
 
     // 3. Set the default title of your dashboard. If found, this template uses the NFT Collection name for the title.
-    "title": "My NFT Collection",
+    "title": "Crypto NFTs",
 
     // 4. Set your Covalent API Key
     "api_key": "ckey_3bdabb7325ee47cc9bcc1e5f3d7",
@@ -16,7 +16,7 @@ export const CONFIG = {
     "timeseries_chart": true,
 
     // 6. (Optional) Set your banner image
-    "banner_picture": "",
+    "https://i.seadn.io/gae/mnDKx3RXrJganEq9NEPIlMtQ-IdF3125FElpyZV-RvQ3Ayq0GH2TbOTC25Bl9NxN6TPY-Tk_jcT9fRtKKlx6ftJZLVsSugqxXWvaeA?auto=format&w=1000": "",
   },
 
   // Supported networks - just for your reference
